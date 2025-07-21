e941ce0b539f5eb0ddb88a87e565d613f45b8041
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-07-21 20:53:48 +0530

    mouse interaction on plane

--- src/utils/three.js
+++ src/utils/three.js
@@ -1,14 +1,11 @@
 import * as THREE from "three";
-
-import vertex from "../shaders/vertex.glsl"
-import fragment from "../shaders/fragment.glsl"
-
+import vertex from "../shaders/vertex.glsl";
+import fragment from "../shaders/fragment.glsl";
 import gsap from "gsap";
 
 export default class Three {
     constructor(options) {
         this.scene = new THREE.Scene();
-
         this.container = options.dom;
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
@@ -19,7 +16,6 @@ export default class Three {
         });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(this.width, this.height);
-        // this.renderer.setClearColor("#111", 1);
         this.renderer.physicallyCorrectLights = true;
         this.renderer.outputEncoding = THREE.sRGBEncoding;
 
@@ -34,23 +30,24 @@ export default class Three {
             100
         );
         this.camera.position.set(0, 0, this.cameraDistance);
-        // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
         this.images = Array.from(document.querySelectorAll("img"));
         this.planes = [];
 
         this.time = 0;
         this.isPlaying = true;
-        this.mouse = 0;
+
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        this.hasMouseMoved = false; // Track if mouse has moved
+        this.hoveredPlane = null; // Track currently hovered plane
 
         this.addObjects();
-        // this.mouseEvent();
-        // this.addPost();
+        this.mouseEvent();
         this.imgPlanes();
         this.resize();
         this.setupResize();
         this.render();
-        //   this.settings();
     }
 
     addObjects() {
@@ -63,10 +60,10 @@ export default class Three {
                 time: { value: 0 },
                 resolution: { value: new THREE.Vector4() },
                 uvRate1: { value: new THREE.Vector2(1, 1) },
+                uMouse: {value : this.mouse}
             },
             vertexShader: vertex,
             fragmentShader: fragment,
-            // wireframe : true,
         });
 
         this.geometry = new THREE.PlaneGeometry(1, 1);
@@ -75,20 +72,11 @@ export default class Three {
     }
 
     imgPlanes() {
-        this.images.forEach(image => {
+        this.images.forEach((image) => {
             const imageBounds = image.getBoundingClientRect();
             const texture = new THREE.TextureLoader().load(image.src);
-            const material = new THREE.ShaderMaterial({
-                vertexShader: vertex,
-                fragmentShader: fragment,
-                uniforms: {
-                    uTexture: { value: texture },
-                    uMouse: { value: new THREE.Vector2(0.5, 0.5) },
-                    uHover: { value: 0 }
-                }
-            });
             const geometry = new THREE.PlaneGeometry(imageBounds.width, imageBounds.height);
-            const plane = new THREE.Mesh(geometry, material);
+            const plane = new THREE.Mesh(geometry, this.material);
             plane.position.set(
                 imageBounds.left - this.width / 2 + imageBounds.width / 2,
                 -imageBounds.top + this.height / 2 - imageBounds.height / 2,
@@ -112,16 +100,51 @@ export default class Three {
         });
     }
 
-    // settings() {
-    //   this.settings = {
-    //       howmuchrgb: 1,
-    //   };
-    //   this.gui = new dat.GUI();
-    //   // this.gui.add(this.settings, "howmuchrgb", 0, 1, 0.01).onChange((value) => {
-    //   //   this.material.uniforms.progress.value = value;
-    //   // });
-    //   this.gui.add(this.settings, "howmuchrgb", 0, 1, 0.01);
-    // }
+    onHover(plane) {
+        let listNums = document.querySelectorAll(".list_num");
+        let listNumHeight = listNums[0].offsetHeight
+        let listTitles = document.querySelectorAll(".list_title");
+        let listTitlesHeight = listTitles[0].offsetHeight
+
+        console.log(listNumHeight , listTitlesHeight)
+        
+        // Only trigger animation if this is a new hover
+        if (this.hoveredPlane !== plane) {
+            this.hoveredPlane = plane; // Update hovered plane
+            gsap.to(`.list_num`, {
+                // y: "2vw",
+                y: listNumHeight / 2,
+                duration: 0.5,
+            });
+            gsap.to(".list_title", {
+                y: -listTitlesHeight / 2,
+                duration: 0.5,
+            });
+        }
+    }
+
+    onHoverEnd() {
+        // Reset animations when hover ends
+        if (this.hoveredPlane) {
+            this.hoveredPlane = null;
+            gsap.to(`.list_num`, {
+                y: 0,
+                duration: 0.5,
+            });
+            gsap.to(".list_title", {
+                y: 0,
+                duration: 0.5,
+            });
+        }
+    }
+
+    mouseEvent() {
+        window.addEventListener('mousemove', (event) => {
+            this.mouse.x = (event.clientX / this.width) * 2 - 1;
+            this.mouse.y = -(event.clientY / this.height) * 2 + 1;
+            this.hasMouseMoved = true; // Mark that mouse has moved
+        });
+    }
 
     setupResize() {
         window.addEventListener("resize", this.resize.bind(this));
@@ -153,6 +176,20 @@ export default class Three {
         this.time += 0.001;
         if (this.material && this.material.uniforms && this.material.uniforms.time) {
             this.material.uniforms.time.value = this.time;
+        }
+
+        // Only check intersections if mouse has moved
+        if (this.hasMouseMoved) {
+            this.raycaster.setFromCamera(this.mouse, this.camera);
+            const intersects = this.raycaster.intersectObjects(this.planes);
+
+            if (intersects.length > 0) {
+                // Call onHover for the first intersected plane
+                this.onHover(intersects[0].object);
+            } else {
+                // No intersections, end hover state
+                this.onHoverEnd();
+            }
         }
 
         this.updatePlanePosition();
