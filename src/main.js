59f483a0a364ff3e627b56939d44cbf031ef7e8d
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-07-24 21:57:54 +0530

    created ui need to make responsive some part

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+// import './style.css'
 import Three from "./utils/three"
 
 // new Three({
@@ -6,3 +6,34 @@ import Three from "./utils/three"
 // });
 
 // alert("ok")
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     let totalBlocks = 15
+
+//     let projectLine = document.querySelector(".projects_lines");
+//     let projectLineWidth = projectLine.offsetWidth;
+//     let oneBlockWidth = projectLineWidth / totalBlocks;
+
+//     let projectLines = document.querySelectorAll(".projects_lines");
+//     projectLines.forEach(proLine => {
+//         proLine.style.gap = (oneBlockWidth * 2) + "px";
+//     });
+
+
+//     const projectContainers = document.querySelectorAll('.project_container');
+//     projectContainers.forEach((element, index) => {
+//         if ((index + 1) % 2 === 1) { 
+//             element.style.marginTop = (oneBlockWidth * 2 - 8) + "px";
+//         }
+//     });
+
+//     let listImgConts = document.querySelectorAll(".list_img_cont");
+//     listImgConts.forEach(imgCont => {
+//         imgCont.style.width = (oneBlockWidth * 6.5) + "px";
+//         imgCont.style.height = (oneBlockWidth * 3.75) + "px";
+//     })
+// });
+    
+// console.log(oneBlockWidth, oneBlockHeight)
